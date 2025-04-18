10736331d92c4fb3fee513f8fff0c93fc0b3d787
Sofiia Barylska <sofiia.barylska@smile-ukraine.com>
2025-04-18 22:21:23 +0300

    Sorting by price test

--- tests/sortingByName.spec.ts
+++ tests/sortingByName.spec.ts
@@ -1,10 +1,10 @@
-import { test, expect } from '@playwright/test'
+import { test} from '@playwright/test'
 import { ProductListPage } from '../pages/productList.page';
 
 [{ sorting: 'Name (A - Z)' },
  { sorting: 'Name (Z - A)' }
 ].forEach(({ sorting }) => {
-    test(`Product correct sorting by ${sorting}`, async ({ page }) => {
+    test(`Products correct sorting by ${sorting}`, async ({ page }) => {
         const productListPage = new ProductListPage(page)
 
         await page.goto('/');
