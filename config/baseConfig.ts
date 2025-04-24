2cd1054461011fea09c1183cee9207db8c27f2ac
Sofiia Barylska <sofiia.barylska@smile-ukraine.com>
2025-04-24 18:28:42 +0300

    add baseConfig file

---
+++ config/baseConfig.ts
@@ -0,0 +1,14 @@
+import { config as dotenvConfig } from "dotenv";
+import { join } from "path";
+import { IBaseConfig } from "../typings/env";
+
+dotenvConfig({
+  path: join(process.cwd(), ".env"),
+});
+
+export const baseConfig: IBaseConfig = {
+  WEB_URL: process.env.WEB_URL!,
+  USER_EMAIL: process.env.USER_EMAIL!,
+  USER_PASSWORD: process.env.USER_PASSWORD!,
+  USER_NAME: process.env.USER_NAME!,
+};
